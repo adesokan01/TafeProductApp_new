84f9fa004efd979257a496185b596a88d4d102f7
001194059 <akeem.adesokan@student.tafesa.edu.au>
2024-09-30 11:47:15 +0930

    Added the delivery charge $25.

---
+++ ProductMaintenance/ProductApps/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProductApps
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Product cProduct;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                cProduct = new Product(Convert.ToDecimal(priceTextBox.Text), Convert.ToInt16(quantityTextBox.Text));
+                cProduct.calTotalPayment();
+                totalPaymentTextBlock.Text = Convert.ToString(cProduct.TotalPayment);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Enter data again", "Data Entry Error");
+            }
+        }
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            productTextBox.Text = "";
+            priceTextBox.Text = "";
+            quantityTextBox.Text = "";
+            totalPaymentTextBlock.Text = "";
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        // Assuming I have a variable called totalPayment
+        double totalPayment = 100.00; 
+        double deliveryCharge = 25.00;
+        double totalCharge = totalPayment + deliveryCharge;
+
+        // Display the total charge in the text block
+        totalChargeTextBox.Text = $"${totalCharge:0.00}";
+
+    }
+}
